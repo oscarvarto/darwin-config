92df635d411a775fd7516d529681aa7d9230c7b3
oscarvarto <oscarvarto@users.noreply.github.com>
2025-08-20 14:17:01 -0600

    Removing broken/wrong stow files

--- stow/lazyvim/.config/.doom.d/config/ai/my-claude-config.el
+++
@@ -1,8 +0,0 @@
-;;; my-claude-config.el -*- lexical-binding: t; no-byte-compile:t; -*-
-
-(use-package claude-code-ide
-  :bind ("C-c C-'" . claude-code-ide-menu) ; Set your favorite keybinding
-  :config
-  (claude-code-ide-emacs-tools-setup)) ; Optionally enable Emacs MCP tools
-
-(provide 'my-claude-config.el)
